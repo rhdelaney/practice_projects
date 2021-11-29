468c0530d37ef312f2a3c989cb3f460a9e12a46a
Raychel <rhdelaney@crimson.ua.edu>
2021-11-29 14:17:33 -0800

    Two more up and done

--- leetcode/977squaresOfASortedArray/squaresOfASortedArray.cpp
+++ leetcode/977squaresOfASortedArray/squaresOfASortedArray.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) {
-
+  vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> progress;
+    for (int i = 0;i<nums.size();i++) {
+        progress.push_back(nums[i]*nums[i]);
     }
+    sort(progress.begin(),progress.end());
+    return progress;
+  }
 };
