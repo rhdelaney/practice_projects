93ed1a23e346e436272737d9ebd4970ab0eb0808
Raychel <rhdelaney@crimson.ua.edu>
2021-12-01 23:18:32 -0800

    from the study plan problems of the day

---
+++ leetcode/713subarrayProductLessThanK/subarrayProductLessThanK.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int prod = 1;
+        int res = 0;
+        int left = 0;
+
+        //traverse through array/each window
+        for (int right = 0; right < nums.size(); right++) {
+            //store in new window
+            prod *= nums[right];
+            //slide the window
+            while(prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+            res += right - left + 1;
+        }
+        return res;
+    }
+};
