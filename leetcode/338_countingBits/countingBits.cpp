57193722aa0dd845485e210ed3564476ef9ce6c9
Raychel <rhdelaney@crimson.ua.edu>
2022-05-08 20:06:00 -0700

    Updated lists of undone, rename some, push a speed problem

--- leetcode/338_countingBits/countingBits.cpp
+++ leetcode/338_countingBits/countingBits.cpp
@@ -6,7 +6,7 @@ public:
           bit_r.push_back(__builtin_popcount(i));
       }*/
       bit_r.push_back(0);  // for num=0
-      if(n==0) return bit_r;
+      if (n == 0) return bit_r;
       for (int i = 1; i <= n; i++) {
           if (i%2 == 0)
             bit_r.push_back(bit_r[i/2]);
