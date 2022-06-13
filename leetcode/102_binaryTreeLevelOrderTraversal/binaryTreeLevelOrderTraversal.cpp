033c4583ff6e608a014307f26cf780c3a2634d1c
Raychel <rhdelaney@crimson.ua.edu>
2022-06-12 18:20:17 -0700

    It's like I get it, but I overthink it and mess up

---
+++ leetcode/102_binaryTreeLevelOrderTraversal/binaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> l;
+    vector<vector<int>> levelOrder(TreeNode* root) {
+      if (!root) {
+          return {};
+      }
+      level(root,0);
+      return l;
+    }
+    void level(TreeNode* root,int lev) {
+        if (!root)
+          return;
+        if (lev == l.size())
+            l.push_back({});
+        l[lev].push_back(root->val);
+        level(root->left, lev+1);
+        level(root->right,lev+1);
+    }
+};
