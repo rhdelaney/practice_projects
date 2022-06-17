6137f3f886e8761f504f44fc7dd09962c7ad0f9e
Raychel <rhdelaney@crimson.ua.edu>
2022-06-16 20:11:36 -0700

    Another long term problem is donezo

--- leetcode/22_generateParentheses/generateParentheses.cpp
+++ leetcode/22_generateParentheses/generateParentheses.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    vector<string> res;
     vector<string> generateParenthesis(int n) {
-
+       backtracking(n, "", 0, 0);
+       return res;
+    }
+    void backtracking(int n, string paren, int open, int closed) {
+        if (paren.length() == n * 2) {
+            res.push_back(paren);
+            return;
+        }
+        if (open < n) {
+            backtracking(n, paren+"(",open+1,closed);
+        }
+        if (closed < open) {
+            backtracking(n, paren+")",open, closed+1);
+        }
     }
 };
